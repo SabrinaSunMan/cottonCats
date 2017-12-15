7a42661be1527d9fa4011783b75db1b04f1189a0
DESKTOP-JM3HH2O\jane1 <jane199141@gmail.com>
2017-12-15 11:31:19 +0800

    no message

---
+++ MeowStore/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MeowStore.Startup))]
+namespace MeowStore
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
